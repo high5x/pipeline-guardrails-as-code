509becb525fdec9dfb0354505ceba50d28cf57d0
high5x <octskyinc@gmail.com>
2026-06-08 10:32:34 -0400

    Feat: Integrate pipeline orchestration and track target terraform assets

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+﻿terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+  }
+}
+provider "aws" { region = "us-east-1" }
+
+resource "aws_s3_bucket" "production_payroll_storage" {
+  bucket = "corp-production-payroll-data-backend"
+}
+resource "aws_s3_bucket_public_access_block" "vulnerable_leak_path" {
+  bucket = aws_s3_bucket.production_payroll_storage.id
+  block_public_acls = false
+}
+resource "aws_ebs_volume" "production_database_vol" {
+  availability_zone = "us-east-1a"
+  size              = 500
+  encrypted         = false
+}
